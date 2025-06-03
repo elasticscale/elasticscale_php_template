4d39d8d0c6c64d486f09409c50cdd675ad65100b
andy-elasticscale <andy@elasticscale.com>
2025-06-03 21:56:25 +0100

    fix: playwright

--- tests/Playwright/homepage.spec.ts
+++ tests/Playwright/homepage.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
 test('Laravel homepage loads', async ({ page }) => {
-  await page.goto('http://localhost:8000');
+  await page.goto('http://localhost:8080');
   await expect(page).toHaveTitle(/Laravel/i);
 });
